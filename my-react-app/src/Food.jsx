d5cb92b35cae4443bb4575bb21c956eea2872bc7
petra milicevic <milicevicpetra7@gmail.com>
2023-12-20 18:36:23 +0100

    first commit

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food() {
+  const food1 = "pizza";
+  const food2 = "tiramisu";
+}
+return (
+  <ul>
+    <li>Lasagne</li>
+    <li>{food1}</li>
+    <li>{food2.toUpperCase()}</li>
+  </ul>
+);
+
+export default Food;
